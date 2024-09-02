54784f8f942c495aff86766f6264716512d98cfe
WesRamox <wesleyramox258@gmail.com>
2024-09-02 20:48:18 -0300

    Requisições POST

--- 04-renderizando-html-dinamico/src/index.js
+++ 04-renderizando-html-dinamico/src/index.js
@@ -3,8 +3,14 @@ const path = require('node:path')
 
 const app = express()
 
+let storedUsers = []
+
+// configuração do EJS
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+
+// configuração do Body
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   const title = 'Homepage'
@@ -12,6 +18,22 @@ app.get('/', (req, res) => {
 
 
   res.render('index', { title, message })
+})
+
+app.get("/formulario", (req, res) => {
+  res.render('form')
+})
+
+app.post("/register", (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+  storedUsers.push({ username, password })
+
+  res.redirect('/usuarios')
+})
+
+app.get("/usuarios", (req, res) => {
+  res.render('users', { users: storedUsers })
 })
 
 const PORT = 3000
